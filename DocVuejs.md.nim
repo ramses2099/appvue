d206f015e87c34646a9529d0f8f4be8310d10771
ramses2099 <ramses2099@gmail.com>
2022-08-29 19:17:23 -0400

    added axios axios-vue material-icons materialized css

---
+++ DocVuejs.md.nim
@@ -0,0 +1,12 @@
+## Project vue
+npm install -g @vue/cli
+
+## Create project appvue
+vue create appvue
+
+## Install packages materialize and axios
+npm install materialize-css@next --save
+npm install material-icons@latest --save
+
+npm install axios
+npm i vue-axios
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import "materialize-css/dist/css/materialize.min.css";
+import "material-icons/iconfont/material-icons.css";
 
-createApp(App).mount('#app')
+createApp(App).use(VueAxios, axios).mount("#app");
